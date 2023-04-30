f1525a00ab08149461ff70866fcbbff39a0b6b57
peter <designing.developer111@gmail.com>
2023-04-30 10:34:59 +0530

    MainActivity drawer layout working

--- app/src/main/java/com/example/orbiscrm/MainActivity.kt
+++ app/src/main/java/com/example/orbiscrm/MainActivity.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.orbiscrm.databinding.ActivityMainBinding
 import com.example.orbiscrm.drawer_activities.EmailActivity
 import com.example.orbiscrm.main_activities.AnalyticsActivity
@@ -22,10 +24,27 @@ class  MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         layout = ActivityMainBinding.inflate(layoutInflater)
         setContentView(layout.root)
-        setSupportActionBar(layout.mainToolbar)
-
+        window.statusBarColor = resources.getColor(R.color.main_activity_status_bar_color)
         toggle = ActionBarDrawerToggle(this,layout.mainDrawer,R.string.open_drawer,R.string.close_drawer)
-        layout.mainDrawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        layout.mainDrawer.addDrawerListener(object:DrawerLayout.DrawerListener{
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                layout.userProfileLinearLayout.visibility= View.INVISIBLE
+
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                Toast.makeText(this@MainActivity,"Drawer opened",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+                layout.userProfileLinearLayout.visibility = View.VISIBLE
+            }
+
+            override fun onDrawerStateChanged(newState: Int) {
+            }
+        })
 
 
 
@@ -35,8 +54,6 @@ class  MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         buildExitDialogue()
-        layout.date.text = layout.date.text.toString()+": Today's date"
-        layout.time.text = layout.time.text.toString()+": Current time"
         layout.mainScrollView.isVerticalScrollBarEnabled= false
         layout.analytics.setOnClickListener{
             var intent = Intent(this,AnalyticsActivity::class.java)
@@ -65,4 +82,8 @@ class  MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private fun service()
+    {
+        Toast.makeText(this,"New Button pressed",Toast.LENGTH_SHORT).show()
+    }
 }

e957de051aeedfe90e315dfceaa91afb1a047f88
peter <designing.developer111@gmail.com>
2023-05-01 16:29:05 +0530

    Splash screen final

--- app/src/main/java/com/example/orbiscrm/SplashScreen.kt
+++ app/src/main/java/com/example/orbiscrm/SplashScreen.kt
@@ -3,17 +3,23 @@ package com.example.orbiscrm
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.example.orbiscrm.authentication.LoginActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
+        window.statusBarColor = Color.TRANSPARENT
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         Handler().postDelayed({
-            var intent = Intent(this,MainActivity::class.java)
+            var intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         },2000)
     }
---
+++ app/src/main/java/com/example/orbiscrm/drawer_activities/DeveloperActivity.kt
@@ -0,0 +1,18 @@
+package com.example.orbiscrm.drawer_activities
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.orbiscrm.R
+
+class DeveloperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_developer_acttivy)
+
+
+
+    }
+}

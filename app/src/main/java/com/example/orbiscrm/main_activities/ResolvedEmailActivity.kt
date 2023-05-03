a54ab722f048961aaf6cdd38bcd843a722997bbc
peter <designing.developer111@gmail.com>
2023-05-03 17:32:06 +0530

    Added needed Activity kotlin files

---
+++ app/src/main/java/com/example/orbiscrm/main_activities/ResolvedEmailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.orbiscrm.main_activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.orbiscrm.R
+
+class ResolvedEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resolved_email)
+    }
+}
